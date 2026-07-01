01c3fe4166ff6b0cc41451b326bb7f7f9026afee
pesh000 <102153662+pesh000@users.noreply.github.com>
2026-07-01 14:40:11 +0300

    Update display.py

--- components/guition_display/display.py
+++ components/guition_display/display.py
@@ -11,22 +11,21 @@ guition_ns = cg.esphome_ns.namespace("guition_display")
 
 GuitionDisplay = guition_ns.class_(
     "GuitionDisplay",
-    cg.Component,
+    cg.PollingComponent,
     display.DisplayBuffer,
 )
 
-CONFIG_SCHEMA = (
-    display.FULL_DISPLAY_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(GuitionDisplay),
-        }
-    )
-    .extend(cv.component_schema)
-)
+CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(GuitionDisplay),
+    }
+).extend(cv.polling_component_schema("1s"))
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[cv.GenerateID()])
 
     await cg.register_component(var, config)
+
+    # ⚠️ КЛЮЧОВА ПРОМЯНА:
     await display.register_display(var, config)
