ca3e698af19fe2d9cc36559b1899a2d19962c388
pesh000 <102153662+pesh000@users.noreply.github.com>
2026-07-01 14:18:04 +0300

    Create display.py

---
+++ components/guition_display/display.py
@@ -0,0 +1,32 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+from esphome.components import display
+
+AUTO_LOAD = ["display"]
+
+DEPENDENCIES = []
+
+guition_ns = cg.esphome_ns.namespace("guition_display")
+
+GuitionDisplay = guition_ns.class_(
+    "GuitionDisplay",
+    cg.PollingComponent,
+    display.DisplayBuffer,
+)
+
+CONFIG_SCHEMA = (
+    display.FULL_DISPLAY_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(GuitionDisplay),
+        }
+    )
+    .extend(cv.polling_component_schema("500ms"))
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[cv.GenerateID()])
+
+    await cg.register_component(var, config)
+    await display.register_display(var, config)
